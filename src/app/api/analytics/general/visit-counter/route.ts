6063edef3421e6a0c95b107a00081ef79e26adf7
Luis <luis.espinoza@bizland.tech>
2025-01-11 00:49:28 -0300

    add visit counter

---
+++ src/app/api/analytics/general/visit-counter/route.ts
@@ -0,0 +1,45 @@
+import { db } from "@/firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+
+	const body = await req.json()
+	try {
+		await setDoc(doc(db, "analytics", "visit-counter"), {
+			// @ts-ignore
+			visits: body.visits
+		});
+
+		return NextResponse.json({
+			status: 200,
+			message: 'Visit counter updated',
+		})
+
+	} catch (e) {
+		console.log("Error", JSON.stringify(e))
+		return NextResponse.json({
+			status: 500,
+			message: 'Error updating visit counter',
+		})
+	}
+}
+
+export async function GET(req: Request) {
+	try {
+		const docRef =  await getDoc(doc(db, "analytics", "visit-counter"))
+
+		if (docRef.exists()) {
+			return NextResponse.json({
+				status: 200,
+				visits: docRef?.data().visits
+			})
+		}
+	} catch (e) {
+		console.log("Error", JSON.stringify(e))
+		return NextResponse.json({
+			status: 500,
+			message: 'Error getting visit counter',
+		})
+	}
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,37 @@
 import { MainArticlesFeed } from "@/components/main-articles-feed/main-articles-feed";
 import { BackgroundFlowFragtal } from "@/components/p5/index";
 import { colors } from "@/theme/colors";
+import { useEffect } from "react";
 
 export default function Home() {
+
+	useEffect(() => {
+		const updateVisitors = async () => {
+			try {
+				let res = await fetch('/api/analytics/general/visit-counter', {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				})
+				res = await res.json()
+				console.log(res?.visits)
+				await fetch('/api/analytics/general/visit-counter', {
+					method: 'POST',
+					body: JSON.stringify({
+						// @ts-ignore
+						visits: res?.visits + 1
+					}),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				})
+			} catch (e) {
+				console.log("Error", JSON.stringify(e))
+			}
+		}
+		updateVisitors()
+	}, [])
 
 	return (
 		<main style={{ backgroundColor: colors.background }}>
